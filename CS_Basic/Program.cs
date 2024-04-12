f201930989cfacd0756e256facac5a4385769525
hyorimKang <hyoggang0327@gmail.com>
2024-04-12 10:09:24 +0900

    #3 자료형의 최소 최대 값 출력

--- CS_Basic/Program.cs
+++ CS_Basic/Program.cs
@@ -16,12 +16,18 @@ namespace CS_Basic
             int a = 200000000;
             int b = 100000000;
             Console.WriteLine(a+b);
-
+             
             //오버플로우 연습2
             Console.WriteLine((long)a + b);
             Console.WriteLine(a+(long)b);
             Console.WriteLine((long)a + (long)b);
 
+
+            // 자료형의 최소 최대 값 출력
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
         }
     }
 }   
